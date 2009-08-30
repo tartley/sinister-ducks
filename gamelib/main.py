234f3911501cf6d63d567d68adfa5114f290f75b
glenn.k.jones <millenniumhand@users.noreply.github.com>
2009-08-30 15:48:50 +0000

    Added instructions

--- gamelib/main.py
+++ gamelib/main.py
@@ -8,12 +8,20 @@ from pyglet.gl import (
     GL_COLOR_BUFFER_BIT, GL_PROJECTION, GL_QUADS, 
 )
 from pyglet.graphics import draw
+from pyglet.text import Label
 from pyglet.window import key, Window
 from pyglet.media import load
 
 from player import Player
 from level import Level
 
+MESSAGES = [
+        "ESC to exit",
+        "Left and right to move around",
+        "Z flaps wings",
+        "Catch feathers to increase flap strength",
+        ""
+]
 
 clockDisplay = clock.ClockDisplay()
 
@@ -25,6 +33,8 @@ class Application(object):
         self.level = Level(self.player)
         self.win = None
         self.keyhandler = None
+        self.label = None
+        self.showing_message = 0
 
 
     def update(self, dt):
@@ -35,6 +45,8 @@ class Application(object):
     def draw(self):
         self.gradient_clear()
         self.level.draw()
+        if self.label:
+            self.label.draw()
         clockDisplay.draw()
 
 
@@ -56,6 +68,15 @@ class Application(object):
             ('c3B', colors),
         )        
 
+    def change_text(self):
+        message = MESSAGES[self.showing_message]
+        self.showing_message += 1
+        print "Label", message
+        self.label = Label(message,
+                font_size=36, x=1024, y=0,
+                anchor_x='right', anchor_y='bottom')
+
+
 
     def run(self):
         self.win = Window(width=1024, height=728)
@@ -68,6 +89,8 @@ class Application(object):
         # music = load(join('data', 'musik.ogg'))
         # clock.schedule_once(lambda _: music.play(), 1)
         clock.schedule(self.update)
+        for number, message in enumerate(MESSAGES):
+            clock.schedule_once(lambda _: self.change_text(), 3*number)
         app.run()
 
 
