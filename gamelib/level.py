92443a88b064480f98c601c860d91b88f60b7e87
Jonathan Hartley <tartley@tartley.com>
2009-08-30 15:32:38 +0000

    wraparound

--- gamelib/level.py
+++ gamelib/level.py
@@ -4,14 +4,26 @@ class Level(object):
 
     def __init__(self, player):
         self.age = 0.0
-        self.player = player
+        self.gameents = [player]
+        self.width = 1024
 
 
     def draw(self):
-        self.player.draw()
+        for ent in self.gameents:
+            ent.draw()
+
+
+    def wraparound(self, ent):
+        if ent.x < -70:
+            ent.x += self.width + 70
+        if ent.x > self.width:
+            ent.x -= self.width + 70
 
 
     def update(self, dt):
         self.age += dt
-        self.player.update(dt)
+        for ent in self.gameents:
+            ent.update(dt)
+            self.wraparound(ent)
+
 
