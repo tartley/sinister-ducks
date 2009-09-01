5d7e1b13cfb4acd93f67af942ba54bb7371bbfdc
Menno Smits <menno@freshfoo.com>
2009-09-01 16:06:26 +0000

    mo-betta feather movement algorithm

--- gamelib/feather.py
+++ gamelib/feather.py
@@ -1,6 +1,7 @@
 
+import math
+from itertools import count
 from random import uniform
-from math import pi, sin
 
 from pyglet import resource
 from pyglet.sprite import Sprite
@@ -20,21 +21,28 @@ class Feather(GameEnt):
         self.sprite = Sprite(resource.image('data/images/feather.png'))
         self.update_sprite_stats(self.sprite)
 
-        self.drift_phase = uniform(0, 2*pi)
-        self.drift_frequency = uniform(0, 2)
-        self.drift_amplitude = 0.5 # uniform(0, 1)
-
+        self.sine_gen = sine_curve()
+        self.saw_gen = saw_curve()
 
     def get_sprite(self):
         return self.sprite
 
 
     def update(self):
+        self.ddx = self.sine_gen.next()
+        self.ddy = self.saw_gen.next()
         GameEnt.update(self)
-        self.ddx +=  self.drift_amplitude * sin(
-            self.level.age * self.drift_frequency + self.drift_phase)
 
 
     def collided_with(self, other):
         pass
 
+
+def sine_curve():
+    to_rad = 2 * math.pi / 360.0
+    for degrees in count():
+        yield math.sin(degrees * 2 * to_rad) / 10
+
+def saw_curve():
+    for t in count():
+        yield ((t % 7) - 3) / 4.0
