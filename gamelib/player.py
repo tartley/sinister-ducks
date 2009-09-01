2c839ab1ae754039460d0579e3e95eabaa5376a0
Jonathan Hartley <tartley@tartley.com>
2009-09-01 12:12:26 +0000

    william's scoring suggestion: score based on consecutive feather collections

--- gamelib/player.py
+++ gamelib/player.py
@@ -22,6 +22,7 @@ class Player(Bird):
     def __init__(self, keyhandler, x, y):
         Bird.__init__(self, x, y)
         self.keyhandler = keyhandler
+        self.consecutive_feathers = 0
 
 
     def think(self):
@@ -37,6 +38,11 @@ class Player(Bird):
 
     def collided_with(self, other):
         Bird.collided_with(self, other)
+
+        if isinstance(other, Bird):
+            self.consecutive_feathers = 0
+
         if isinstance(other, Feather) and other.owner is not self:
-            self.level.score += 10
+            self.consecutive_feathers += 1
+            self.level.score += self.consecutive_feathers
 
