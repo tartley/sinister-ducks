f0213a06cab9b708e4d7a7bdbfc9fc4aa1364922
Jonathan Hartley <tartley@tartley.com>
2009-11-08 10:30:33 +0000

    add 'init() to sounds.py, instead of loading sounds on import

--- gamelib/sounds.py
+++ gamelib/sounds.py
@@ -14,29 +14,33 @@ sounds_dir = join('data', 'sounds')
 sounds = {}
 
 
-def load_sound(name):
+def _load_sound(name):
     return load(join(sounds_dir, name), streaming=False)
 
 
-def load_sounds_matching(pattern):
+def _load_sounds_matching(pattern):
     names = glob(join(sounds_dir, pattern))
     names.sort()
     return [load(n, streaming=False) for n in names]
 
 
 def play(name, index=None):
+    if len(sounds) == 0:
+        init()
     if settings.get('all', 'force_audio') == 'silent':
         return
     if index is None:
-        index = randint(0, len(sounds[name]))
-    index = min(index, len(sounds[name]) - 1)
+        index = randint(0, len(sounds[name]) - 1)
+    else:
+        index = min(index, len(sounds[name]) - 1)
     sounds[name][index].play()
 
 
-if settings.get('all', 'force_audio') != 'silent':
-    sounds['quack'] = load_sounds_matching('quack?.ogg')
-    sounds['ding'] = load_sounds_matching('ding?.ogg')
-    sounds['die'] = load_sounds_matching('die?.ogg')
-    sounds['ohno'] = [load_sound('ohno.ogg')]
-    sounds['flap'] = [load_sound('flap.ogg')]
+def init():
+    if settings.get('all', 'force_audio') != 'silent':
+        sounds['quack'] = _load_sounds_matching('quack?.ogg')
+        sounds['ding'] = _load_sounds_matching('ding?.ogg')
+        sounds['die'] = _load_sounds_matching('die?.ogg')
+        sounds['ohno'] = [_load_sound('ohno.ogg')]
+        sounds['flap'] = [_load_sound('flap.ogg')]
 
