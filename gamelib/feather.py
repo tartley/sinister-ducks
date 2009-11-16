911e512cc35731019a31a05be2b18cf9ff8d327e
Jonathan Hartley <tartley@tartley.com>
2009-11-16 23:29:35 +0000

    feather's owners reset after a second, so you can now re-collect your own feathers

--- gamelib/feather.py
+++ gamelib/feather.py
@@ -2,7 +2,7 @@
 from math import atan2, cos, pi, sin, sqrt
 from random import uniform
 
-from pyglet import resource
+from pyglet import clock, resource
 from pyglet.sprite import Sprite
 
 from worlditem import GRAVITY, WorldItem
@@ -17,11 +17,16 @@ class Feather(WorldItem):
     can_fall_off = True
 
     def __init__(self, x, y, dx, dy, owner):
-        self.owner = owner
         WorldItem.__init__(self, x, y, dx, dy)
         self.rotation = uniform(-0.5, 0.5)
         self.speed = uniform(5, 12)
         self.curve = uniform(0.002, 0.02)
+        self.owner = owner
+        clock.schedule_once(lambda _: self.reset_owner(), 1)
+
+
+    def reset_owner(self):
+        self.owner = None
 
 
     def update(self):
