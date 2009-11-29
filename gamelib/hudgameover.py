45a60a63a61c263c487c4faf6dcaaa6e30eec293
Jonathan Hartley <tartley@tartley.com>
2009-11-29 11:36:26 +0000

    game over goes away after 4 seconds, leading straight back to title screen. keypress still skips straight there.

--- gamelib/hudgameover.py
+++ gamelib/hudgameover.py
@@ -4,9 +4,12 @@ from hudmessage import HudMessage
 class HudGameOver(HudMessage):
 
     def __init__(self):
-        HudMessage.__init__(self, 'Game Over', remove_after=None)
+        HudMessage.__init__(self, 'Game Over', remove_after=4)
+
+    def removed(self):
+        HudMessage.removed(self)
+        self.game.title()
 
     def on_key_press(self, _, __):
         self.remove_from_game = True
-        self.game.title()
 
